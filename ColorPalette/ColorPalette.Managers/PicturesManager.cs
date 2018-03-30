71f6a8d507eab380f6198b7d36492ac64c01b43c
Archer McNeill <rr606210@fincoad.com>
2018-03-30 18:34:00 -0500

    add initial code for transforming RGB to HSV and sorting based on hue

--- ColorPalette/ColorPalette.Managers/PicturesManager.cs
+++ ColorPalette/ColorPalette.Managers/PicturesManager.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using ColorPalette.Managers.Interfaces;
+﻿using ColorPalette.Managers.Interfaces;
 using ColorPalette.Objects.DTOs;
 using ColorPalette.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ColorPalette.Managers
 {
     public class PicturesManager : IPicturesManager
     {
+        private const int SCALE = 255;
         private readonly IPicturesRepository _picturesRepository;
 
         public PicturesManager(IPicturesRepository picturesRepository)
@@ -27,7 +33,8 @@ namespace ColorPalette.Managers
 
         public async Task<PictureDTO> AddPicture(PictureDTO picture)
         {
-            picture.ColorSwaths = GenerateColorSwaths(picture.Contents);
+            using (Bitmap image = (Bitmap)Image.FromStream(new MemoryStream(picture.Contents)))
+                picture.ColorSwaths = GenerateColorSwaths(image);
 
             return await _picturesRepository.AddAsync(picture);
         }
@@ -39,9 +46,73 @@ namespace ColorPalette.Managers
 
         #region Helper Methods
 
-        private List<int[]> GenerateColorSwaths(byte[] contents)
+        private List<int[]> GenerateColorSwaths(Bitmap image)
         {
-            return null;
+            var bmpData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
+
+            //var hsvEntries = colorPalette.Entries.Select(ConvertRGBToHSV).ToList();
+            //var sortedEntries = SortAndSeparateHsvValues(hsvEntries);
+
+            return new List<int[]>();
+        }
+
+        private Hsv ConvertRGBToHSV(Color pixel)
+        {
+            // find the percent value of each color
+            var red = pixel.R / SCALE;
+            var green = pixel.G / SCALE;
+            var blue = pixel.B / SCALE;
+
+            // find the minimum and maximum of the percentage values
+            var max = Math.Max(red, Math.Max(green, blue));
+            var min = Math.Min(red, Math.Min(green, blue));
+
+            // get the change between the min and the max
+            var delta = max - min;
+
+            // get ready to set the raw hue before it is scaled
+            var rawHue = 0;
+
+            // operate differently on hue depending on which color is the max
+            if (max == red)
+                rawHue = (green - blue) / delta;
+            else if (max == green)
+                rawHue = ((blue - red) / delta) + 2;
+            else if (max == blue)
+                rawHue = ((red - green) / delta) + 4;
+
+            // scale hue
+            var hue = rawHue * SCALE;
+
+            // scale the maximum color to get value
+            var value = max * SCALE;
+
+            // find the saturation, which is 0 if value is 0 else it's delta - value which we then scale
+            var saturation = value == 0 ? 0 :
+                (delta / value) * SCALE;
+
+            // return our lovely HSV object
+            return new Hsv { Hue = hue, Saturation = saturation, Value = value };
+        }
+
+        private List<List<Hsv>> SortAndSeparateHsvValues(List<Hsv> values)
+        {
+            if (values == null || values.Count == 1)
+                return null;
+
+            // sort HSV values by Hue first off
+             values.Sort((a,b) => a.Hue.CompareTo(b.Hue));
+
+            return new List<List<Hsv>>();
+        }
+
+        // make a kiddie shit POCO to hold our values
+        private class Hsv
+        {
+            public int Hue { get; set; }
+            public int Saturation { get; set; }
+            public int Value { get; set; }
         }
 
         #endregion
